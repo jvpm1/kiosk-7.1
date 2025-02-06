96d0b766d33eb24c325449e08fb4f8d0e9aac48c
GenoKooijman <genokooijman@gmail.com>
2025-02-06 12:49:46 +0100

    tailwind/vite installed, basic setup

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import postcss from '@tailwindcss/postcss';
+
+export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [
+        postcss()
+      ]
+    }
+  }
+});
