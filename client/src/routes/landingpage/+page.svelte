77fb486340b1adc1e1c07957cab2accb39ba352b
GenoKooijman <genokooijman@gmail.com>
2025-02-12 11:21:15 +0100

    menu images

--- client/src/routes/landingpage/+page.svelte
+++ client/src/routes/landingpage/+page.svelte
@@ -1,3 +1,9 @@
-<div id="">
-
-</div>
+<div class="bg-green-600 rounded p-1"><a href="/" class="text-4xl  rounded p-1">Eat in</a></div>
+<div id="ProductsContainer" class="grid grid-cols-3 grid-rows-6">
+    <div class="col-start-1 row-start-1 flex flex-col items-center">test1</div>
+    <div class="col-start-2 row-start-1">test2</div>
+    <div class="col-start-3 row-start-1">test3</div>
+    <div class="col-start-1 row-start-2">test4</div>
+    <div class="col-start-2 row-start-2">test5</div>
+    <div class="col-start-3 row-start-2">test6</div>
+  </div>
